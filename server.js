e0571b6c654110dba2ec57fb6124d7bbcbd2d571
costa-rica <nrodrig1@gmail.com>
2024-09-18 06:41:13 +0200

    various changes to server and css

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const config = require('./config');
 require('./models/connection');
 const stockPrice = require('./models/stockPrices')
 const apiFetchStockData = require('./analysis/apiFetchPrices');
-const {createDailyPriceWithRsiArray,simulateBuyAndSell} = require('./analysis/createDailyPriceArray');
+const { createDailyPriceWithRsiArray, simulateBuyAndSell } = require('./analysis/createDailyPriceArray');
 
 // To make server
 const express = require('express')
@@ -14,25 +14,31 @@ const path = require('path');
 app.use(express.static('public'))
 
 
-app.get("/", (req,res)=>{
-    const tickerSymbol = "BOIL"
-    createDailyPriceWithRsiArray(tickerSymbol).then(arryStock =>{
-    
-        const arraySimulatedDailyAction = simulateBuyAndSell(arryStock, 30, 70, 10000,"2019-01-01")
+app.route("/").get( (req, res) => {
+        const tickerSymbol = "BOIL"
+        createDailyPriceWithRsiArray(tickerSymbol).then(arryStock => {
 
-        // Format date and RSI
-        for (let elem of arraySimulatedDailyAction){
-            elem.date = elem.date.toISOString().split('T')[0]
-            elem.rsi = Math.round(elem.rsi * 100) /100
-            elem.cash = Math.round(elem.cash * 100) /100
-        }
-        const options = {arraySimulatedDailyAction:arraySimulatedDailyAction, tickerSymbol:tickerSymbol}
-        res.render("index", options);
+            const arraySimulatedDailyAction = simulateBuyAndSell(arryStock, 30, 70, 10000, "2019-01-01")
+
+            // Format date and RSI
+            for (let elem of arraySimulatedDailyAction) {
+                elem.date = elem.date.toISOString().split('T')[0]
+                elem.rsi = Math.round(elem.rsi * 100) / 100
+                elem.cash = Math.round(elem.cash * 100) / 100
+            }
+            const options = { arraySimulatedDailyAction: arraySimulatedDailyAction, tickerSymbol: tickerSymbol }
+            res.render("index", options);
+        })
     })
-});
+    .post((req,res)=>{
+        console.log("- post")
+        console.log(`req: ${req}`)
+        res.render("index", options);
+    });
+
 
 
 
 app.listen(config.PORT, () => {
-  console.log(`Server is running on port ${config.PORT}`);
+    console.log(`Server is running on port ${config.PORT}`);
 });
